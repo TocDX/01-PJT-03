2031d9e6bbc463e3807c26176e7a7dc6d0e32ad5
TocDX <sk372021000@gmail.com>
2022-07-29 17:25:51 +0900

    모의고사

--- 3회차/서민수/2_문자열의거울상.py
+++ 3회차/서민수/2_문자열의거울상.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_문자열의거울상.txt")
+# 문제를 보면 떠오는건 reverse 혹은 역순?
+# 또는 슬라이싱으로..?
+# 또는 규칙을 찾아내는 것?
+# 규칙이 있다면 어떤것일까
+# qqqqpppbbd, bddqqqpppp
+# q와p d와b가 바뀌는게 아닐까하고 생각하면
+# q부터 시작하는게 아니라 d부터 시작 해서 
+# dbbpppqqqq 이지 않을까 생각한다
+# 역순을 해서 바꾼것 같다
+
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    word =input()
+    for i in word:
--- 3회차/서민수/3_직사각형길이찾기.py
+++ 3회차/서민수/3_직사각형길이찾기.py
@@ -1,3 +1,22 @@
 import sys
+# a,b,c가 있을때 1 2 1로 입력받으면
+# 나머지 숫자는 2로 된다 만약 세 숫자가 같으면 나머지 숫자도 같게 된다
+# 그렇다면 사용할 수 있는건 조건문이라고 생각한다.
+# a와 b가 같지 않다면 d와 c는 a와b랑 무조건 붙어있으니
+# a+b-c하면 d가 나온다고 생각한다
+# 나오는 값을 리스트에 저장한다
 
 sys.stdin = open("_직사각형길이찾기.txt")
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+
+for test_case in range(1, T + 1):
+    a, b, c, = map(int,input().split())
+    lst = [a,b,c]
+
+    if a != b:
+        d = a+b-c
+    else:
+        d = c
+        lst.append(d)
+    print("#{} {}".format(test_case,d))
